ccc61978b11796b64ed4086837593f0141ca6aff
Rawan Rashed <rawan@live.ru>
2019-08-25 13:26:29 +0100

    Add files via upload

---
+++ ReadImagesFromHDF5File.py
@@ -0,0 +1,92 @@
+# Reference: http://machinelearninguru.com/deep_learning/data_preparation/hdf5/hdf5.html
+import h5py
+import numpy as np
+from random import shuffle
+from math import ceil
+import matplotlib.pyplot as plt
+import os
+
+
+# we might need os.sep in the path in linux OS
+BASE_DIR = os.path.join("..", "Downloads","dogs-vs-cats", "train")
+
+DatasetName = "CatsVSDogs.hdf5" # path+name of the new hdf5 file
+ImagesToTrain = os.path.join(BASE_DIR,"*.jpg")#path to the images you want to convert to hdf5 tables
+
+hdf5_path = DatasetName  
+dataToTrainPath = ImagesToTrain
+hdf5_file = h5py.File(hdf5_path, "r")
+
+
+def load_data():
+	train_dataset = h5py.File(hdf5_path, "r")
+	train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+	train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+	
+	test_dataset = hdf5_file
+	test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+	test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+	classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+	
+	train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+	test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+ 
+	return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+    
+    
+
+def training_data_number():
+	hdf5_file = h5py.File(hdf5_path, "r")
+	data_num = hdf5_file["train_set_x"].shape[0]
+	return data_num
+	
+
+
+
+def create_batches_list():
+
+	batch_size = 10 # number of training samples in each batch
+	
+	data_num = training_data_number()
+	# create list of batches to shuffle the data
+	batches_list = list(range(int(ceil(float(data_num) / batch_size))))
+	
+	shuffle(batches_list)
+	
+	hdf5_file = h5py.File(hdf5_path, "r")
+	# loop over batches
+	for n, i in enumerate(batches_list):
+		i_s = i * batch_size  # index of the first image in this batch
+		i_e = min([(i + 1) * batch_size, data_num])  # index of the last image in this batch
+
+		# read batch images and remove training mean
+		
+		images = hdf5_file["train_set_x"][i_s:i_e, ...]
+
+
+		# read labels and convert to one hot encoding 
+		# https://machinelearningmastery.com/why-one-hot-encode-data-in-machine-learning/
+		# https://machinelearningmastery.com/how-to-one-hot-encode-sequence-data-in-python/
+		num_class = hdf5_file ["list_classes"][:].size
+		labels = hdf5_file["train_set_y"][i_s:i_e]
+		labels_one_hot = np.zeros((batch_size, num_class))
+		labels_one_hot[np.arange(batch_size), labels] = 1
+
+		print(str(n+1) + "/" + str(len(batches_list)))
+		print(str(labels[0]) + str(labels_one_hot[0, :]))
+	
+		plt.imshow(images[0])
+		plt.show()
+
+		if n == 5:  # break after 5 batches
+			break
+	return batches_list
+
+hdf5_file.close()
+
+def main():
+	create_batches_list()
+	
+if __name__ == "__main__":
+    main()
---
+++ SaveImagesToHDF5File.py
@@ -0,0 +1,162 @@
+#Reference: http://machinelearninguru.com/deep_learning/data_preparation/hdf5/hdf5.html
+#In this file I have not put any function because we only need to run this file for once to save the data
+from random import shuffle
+import glob
+import numpy as np
+import h5py
+import cv2
+import os
+
+
+# we might need os.sep in the path in linux OS
+BASE_DIR = os.path.join("..", "Downloads","dogs-vs-cats", "train")
+
+DatasetName = "CatsVSDogs.hdf5" # path+name of the new hdf5 file
+ImagesToTrain = os.path.join(BASE_DIR,"*.jpg")#path to the images you want to convert to hdf5 tables
+
+hdf5Path = DatasetName  
+dataToTrainPath = ImagesToTrain
+
+classes_names = ["dog","cat"]
+
+# read images' names which include their labels from the 'train' folder
+imagesNames = glob.glob(dataToTrainPath)
+
+labels = []
+for name in imagesNames:
+	if ("cat" in (name.split("/")[-1])): #to test the name of the image without the path; just taking the last part of image
+		labels.append(1) # in case the image is cat give it label 1
+	else:
+		labels.append(0) # in case the image is dog give it label 0
+
+
+# shuffle_data = True
+# 
+# # # to shuffle data
+# if shuffle_data:
+# 	c = list(zip(imagesNames, labels))
+#     shuffle(c)
+#     imagesNames, labels = zip(*c)
+
+# Divide the data into 60% train, 20% validation, and 20% test
+trainImagesNames = imagesNames[0:int(0.6 * len(imagesNames))]
+trainLabels = labels[0:int(0.6 * len(labels))]
+
+valImagesNames = imagesNames[int(0.6 * len(imagesNames)):int(0.8 * len(imagesNames))]
+valLabels = labels[int(0.6 * len(imagesNames)):int(0.8 * len(imagesNames))]
+
+testImagesNames = imagesNames[int(0.8 * len(imagesNames)):]
+testLabels = labels[int(0.8 * len(labels)):]
+
+
+
+
+data_order = 'tf'  # 'th' for Theano, 'tf' for Tensorflow
+
+# check the order of data and chose proper data shape to save images
+if data_order == 'th':
+    trainShape = (len(trainImagesNames), 3, 224, 224)
+    valShape = (len(valImagesNames), 3, 224, 224)
+    testShape = (len(testImagesNames), 3, 224, 224)
+elif data_order == 'tf':
+    trainShape = (len(trainImagesNames), 224, 224, 3)
+    valShape = (len(valImagesNames), 224, 224, 3)
+    testShape = (len(testImagesNames), 224, 224, 3)
+
+# open a hdf5 file and create earrays
+hdf5File = h5py.File(hdf5Path, mode='w')
+
+hdf5File.create_dataset("train_set_x", trainShape, np.int8)
+hdf5File.create_dataset("val_set_x", valShape, np.int8)
+hdf5File.create_dataset("test_set_x", testShape, np.int8)
+
+hdf5File.create_dataset("train_mean", trainShape[1:], np.float32)
+
+
+#we have two classes here dog and cat
+# to save the names of classes as string ex. dog and cat
+dt = h5py.special_dtype(vlen=str)
+hdf5File.create_dataset("list_classes", (1,len(classes_names)), dtype=dt) 
+hdf5File["list_classes"][...]=classes_names
+
+hdf5File.create_dataset("train_set_y", (len(trainImagesNames),), np.int8)
+hdf5File["train_set_y"][...] = trainLabels
+hdf5File.create_dataset("val_set_y", (len(valImagesNames),), np.int8)
+hdf5File["val_set_y"][...] = valLabels
+hdf5File.create_dataset("test_set_y", (len(testImagesNames),), np.int8)
+hdf5File["test_set_y"][...] = testLabels
+
+
+
+# a numpy array to save the mean of the images
+mean = np.zeros(trainShape[1:], np.float32)
+
+# loop over train nameesses
+for i in range(len(trainImagesNames)):
+    # print how many images are saved every 1000 images
+    if i % 1000 == 0 and i > 1:
+    	print ("Train data:" + str(i) + "/" + str(len(trainImagesNames)))
+
+    # read an image and resize to (224, 224)
+    # cv2 load images as BGR, convert it to RGB
+    name = trainImagesNames[i]
+    img = cv2.imread(name)
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # add any image pre-processing here
+
+    # if the data order is Theano, axis orders should change
+    if data_order == 'th':
+        img = np.rollaxis(img, 2)
+
+    # save the image and calculate the mean so far,  None is a list of one item
+    hdf5File["train_set_x"][i, ...] = img[None]
+    # the mean is calculated for training data only
+    mean += img / float(len(trainLabels))
+
+# loop over validation nameesses
+for i in range(len(valImagesNames)):
+    # print how many images are saved every 1000 images
+    #if i % 1000 == 0 and i > 1:
+    #     print ("Validation data:" + str(i) + "/" + str(len(valImagesNames)))
+    # read an image and resize to (224, 224)
+    # cv2 load images as BGR, convert it to RGB
+    name = valImagesNames[i]
+    img = cv2.imread(name)
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # add any image pre-processing here
+
+    # if the data order is Theano, axis orders should change
+    if data_order == 'th':
+        img = np.rollaxis(img, 2)
+
+    # save the image, None is a list of one item
+    hdf5File["val_set_x"][i, ...] = img[None]
+
+# loop over test nameesses
+for i in range(len(testImagesNames)):
+    # print how many images are saved every 1000 images
+    #if i % 1000 == 0 and i > 1:
+    #	print ("Test data:" + str(i) + "/" + str(len(testImagesNames)))
+    # read an image and resize to (224, 224)
+    # cv2 load images as BGR, convert it to RGB
+    name = testImagesNames[i]
+    img = cv2.imread(name)
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # add any image pre-processing here
+
+    # if the data order is Theano, axis orders should change
+    if data_order == 'th':
+        img = np.rollaxis(img, 2)
+
+    # save the image,  None is a list of one item
+    hdf5File["test_set_x"][i, ...] = img[None]
+
+# save the mean and close the hdf5 file
+hdf5File["train_mean"][...] = mean
+hdf5File.close()
